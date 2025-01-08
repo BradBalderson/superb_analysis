63d47da81ed72e1e6f2b385f90f97f93d99a1124
Mickey Lorenzini <mhlorenzini@gmail.com>
2025-01-08 00:11:53 -0800

    Add files via upload
    Scripts to run Split-pipe

---
+++ scripts/split_pipe/split_pipe_10k_cell.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Task ID range (e.g. 1-6)
+#$ -t 1
+# Max parallel tasks
+#$ -tc 1
+# Concatenate stdout, stderr
+#$ -j y
+# Job name
+#$ -N spipe-10k
+# Job stdout directory
+#$ -o # [OMITTED]
+
+# SPLIT-PIPE PROCESSING OF SUPERB-SEQ READS
+# 10K CELL LIBRARY
+# Split-pipe v1.1.1
+# Run time ~15 hours with 16 threads
+
+# Run name
+runName="split_pipe_10k_cell"
+# Parent directory
+parentDir= # [OMITTED]
+# Input file paths (input cBC.bam)
+inDir= # [OMITTED]
+# Output directory
+outDir= # [OMITTED]
+# BWA index directory
+hg38Dir= # [OMITTED]
+
+# Source environment
+source # [OMITTED]
+mamba activate # [ENV NAME]
+# Make variables
+inR1=$inDir/*R1.fastq.gz
+inR2=$inDir/*R2.fastq.gz
+
+# SPLIT-PIPE COMMAND
+# --mode all,     Run all pipeline steps
+# --nthreads 16,  Threads
+# --chemistry v2, Kit version
+# --sample,       Sample names and bacroding round 1 well ranges
+date
+echo "Running split-pipe..."
+echo "Processing $inR1 and $inR2..."
+split-pipe \
+    --mode all --nthreads 16 --chemistry v2 \
+    --genome_dir $hg38Dir --output_dir $outDir \
+    --fq1 $inR1 --fq2 $inR2 \
+    --sample no_ki A1-A2 \
+    --sample baf_ki A3-A7 \
+    --sample nurd_ki A8-A12
+echo "Done"
+date
